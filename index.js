281dc97fc30a8a79ef55999d78def035987f4c35
Sheikh Mohammed Umar <sheikhmdumar.ec19@rvce.edu.in>
2022-05-30 15:58:23 +0530

    Initialize

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hii Umar,u got it");
 });
-
+app.get("/umar", (req, res) => {
+  res.send("Hii Umar,u double got it");
+});
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
